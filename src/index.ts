5422e68eb86fc4291f2738f27e4ec5b09a356687
Tarindhi <tharidithisarani7@gmail.com>
2025-06-28 16:06:09 +0530

    installing the nodemon and run

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,7 @@ const port = 3000;
 
 //  3. Define a simple HTTP Get Request
 app.get('/', (req: Request, res: Response) => {
-    res.send("Hello");
+    res.send("Hello Tharidi,  How are you....!");
 });
 
 //  4. Instructs the express app to listen on port 3000
