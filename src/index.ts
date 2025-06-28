6b1330a22756660c0118cc2da712c697a9b3c9c7
Tarindhi <tharidithisarani7@gmail.com>
2025-06-28 15:04:37 +0530

    port lisning

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,17 @@
-import express, {Express} from 'express';
+import express, {Express, Request, Response} from 'express';
 
-// 1. Initialize the express app
+//  1. Initialize the express app
 const app : Express = express();
+
+//  2. Define the application port
+const port = 3000;
+
+//  3. Define a simple HTTP Get Request
+app.get('/', (req: Request, res: Response) => {
+    res.send("Hello");
+});
+
+//  4. Instructs the express app to listen on port 3000
+app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`)
+});
